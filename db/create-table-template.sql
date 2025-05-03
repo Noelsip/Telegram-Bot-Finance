a723db0ac97b1441cd6a7281b63d9547a73c2a35
Noelsip <noelsipayung45@gmail.com>
2025-05-04 02:08:25 +0800

    second Commit

--- db/create-table-template.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE transaksi(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    jenis VARCHAR(20),
-    jumlah INT,
-    tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    catatan TEXT
-);
